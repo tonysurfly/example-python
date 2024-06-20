dc3716de9f2c050e36bab863e58bd5c4cad379b4
Tony Pushmore <tony@surfly.com>
2024-06-20 09:38:32 +0200

    ffs

--- app/calculator.py
+++ app/calculator.py
@@ -1,5 +1,4 @@
 class Calculator:
-
     def add(x, y):
         return x + y
 
@@ -11,5 +10,30 @@ class Calculator:
 
     def divide(x, y):
         if y == 0:
-            return 'Cannot divide by 0'
+            return "Cannot divide by 0"
         return x * 1.0 / y
+
+    def power(x, y):
+        return x**y
+
+    def root(x, y):
+        return x ** (1.0 / y)
+
+    def factorial(x):
+        if x == 0:
+            return 1
+        return x * Calculator.factorial(x - 1)
+
+    def calculate_sum(n):
+        # Code complexity
+        result = 0
+        for i in range(n):
+            result += i * i + 1
+
+        # Duplication
+        total_sum = result + result
+
+        # Unused variable
+        unused_var = 10
+
+        return total_sum
